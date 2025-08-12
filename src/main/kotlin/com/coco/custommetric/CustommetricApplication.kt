125ca0fb3e89e547d496b5b9195b49feb874c997
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2025-08-12 23:22:53 +0900

    init

---
+++ src/main/kotlin/com/coco/custommetric/CustommetricApplication.kt
@@ -0,0 +1,11 @@
+package com.coco.custommetric
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustommetricApplication
+
+fun main(args: Array<String>) {
+	runApplication<CustommetricApplication>(*args)
+}
---
+++ src/test/kotlin/com/coco/custommetric/CustommetricApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coco.custommetric
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustommetricApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
