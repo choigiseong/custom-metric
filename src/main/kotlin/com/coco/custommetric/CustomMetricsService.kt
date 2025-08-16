e943e9a0891fdd34be3f204bd5b3a8a60bf9b4c5
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2025-08-16 15:49:30 +0900

    update Gauge
    - 게이지 추가

--- src/main/kotlin/com/coco/custommetric/CustomMetricsService.kt
+++ src/main/kotlin/com/coco/custommetric/CustomMetricsService.kt
@@ -1,13 +1,21 @@
 package com.coco.custommetric
 
 import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.stereotype.Service
+import java.time.Instant
 
 @Service
 class CustomMetricsService(
     private val meterRegistry: MeterRegistry
 ) {
+    init {
+        Gauge.builder("inventory") { Instant.now().toEpochMilli() }
+            .tag("productId", "1")
+            .description("Current stock of product")
+            .register(meterRegistry)
+    }
 
     fun incrementOrderCount(productId: String) {
         val orderCounter = meterRegistry.counter(
@@ -16,5 +24,7 @@ class CustomMetricsService(
         )
         orderCounter.increment()
     }
+
+
 
 }
